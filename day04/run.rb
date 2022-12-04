f7a50352f48a64a1641d8159f8c9bb1a5797ed3f
Thomas Lulé <thomas@lule.fr>
2022-12-04 11:31:59 +0100

    feat: day 4 part 2

--- day04/run.rb
+++ day04/run.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class RangesPair
-  attr_accessor :range1, :range2
-
   def initialize(line)
     @range1, @range2 = line.split(",").map do |range_str|
       from, to = range_str.split("-")
@@ -13,13 +11,20 @@ class RangesPair
   def fully_contained?
     @range1.all? { |from1| @range2.include?(from1) } || @range2.all? { |from2| @range1.include?(from2) }
   end
+
+  def overlap?
+    @range1.any? { |from1| @range2.include?(from1) } || @range2.any? { |from2| @range1.include?(from2) }
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   fully_contained_count = 0
+  overlap_count = 0
   File.readlines("input.txt", chomp: true).each do |line|
     pair = RangesPair.new(line)
     fully_contained_count += 1 if pair.fully_contained?
+    overlap_count += 1 if pair.overlap?
   end
-  puts fully_contained_count
+  puts "part 1 #{fully_contained_count}"
+  puts "part 2 #{overlap_count}"
 end
